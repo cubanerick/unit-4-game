34b83c771fa4a20cb06c632ad58da286c8c7fd09
cubanerick <37166192+cubanerick@users.noreply.github.com>
2018-05-21 23:42:18 -0700

    mid way

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
     var wins = 0;
     var losses = 0;
     var randomNum = Math.floor(Math.random()*(102)+19)
+    var totalScore = 0;
 
-    $("img").on("click", function() {
+
+    $("img").on("click", function(){
         
         $("#crystal1").attr("value",Math.floor(Math.random()*(12)+1));
         $("#crystal2").attr("value",Math.floor(Math.random()*(12)+1));
@@ -12,5 +14,24 @@ $(document).ready(function(){
         $("#crystal4").attr("value",Math.floor(Math.random()*(12)+1));
 
         console.log(event.target);
+    });
+
+   
+
+    $("#crystal1").on("click", function(){
+        totalScore = totalScore + $("#crystal1").attr("value");
+        console.log(totalScore);
+    });
+    $("#crystal2").on("click", function(){
+        totalScore = totalScore + $("#crystal2").attr("value");
+        console.log(totalScore);
+    });
+    $("#crystal3").on("click", function(){
+        totalScore = totalScore + $("#crystal3").attr("value");
+        console.log(totalScore);
+    });
+    $("#crystal4").on("click", function(){
+        totalScore = totalScore + $("#crystal4").attr("value");
+        console.log(totalScore);
     });
 });
